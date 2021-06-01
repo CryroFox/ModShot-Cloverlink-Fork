f235ddd28422faa4f1495fd96b5028511bce527b
CryroFox <44964856+CryroFox@users.noreply.github.com>
2021-05-31 18:05:04 -0700

    Turned off path cache in an attempt to see if this allows for on the fly file downloading

--- src/config.cpp
+++ src/config.cpp
@@ -96,7 +96,7 @@ void Config::read(int argc, char *argv[])
 	PO_DESC(iconPath, std::string, "") \
 	PO_DESC(SE.sourceCount, int, 6) \
 	PO_DESC(audioChannels, int, 30) \
-	PO_DESC(pathCache, bool, true) \
+	PO_DESC(pathCache, bool, false) \
 
 // Not gonna take your shit boost
 #define GUARD_ALL( exp ) try { exp } catch(...) {}
